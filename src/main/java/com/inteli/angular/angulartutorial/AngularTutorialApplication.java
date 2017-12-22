8061ac3440b995d61409bedd20e055ab224d8ba2
Artur Pietrucha <artur.pietrucha@nexio.pl>
2017-12-22 12:22:25 +0100

    first commit

---
+++ src/main/java/com/inteli/angular/angulartutorial/AngularTutorialApplication.java
@@ -0,0 +1,14 @@
+package com.inteli.angular.angulartutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan
+public class AngularTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularTutorialApplication.class, args);
+	}
+}
---
+++ src/main/java/com/inteli/angular/angulartutorial/controllers/TestController.java
@@ -0,0 +1,18 @@
+package com.inteli.angular.angulartutorial.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
+
+@RestController
+public class TestController {
+
+    private static final Logger LOGGER = Logger.getLogger(TestController.class.getName());
+
+    @RequestMapping("/api/hi")
+    public String hi() {
+        LOGGER.info("I am inside controller");
+        return "I am test home page from rest controller";
+    }
+}
